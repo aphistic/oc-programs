94c58b150e47f4620539a0b3247ce1eb6901232f
Erik Davidson <erik@erikd.org>
2018-12-16 21:49:36 -0600

    Fix some names

--- aphstore/bin/aphstore.lua
+++ aphstore/bin/aphstore.lua
@@ -10,19 +10,21 @@ trash_names = {
 }
 
 
-scanner_side = -1
+inv_side = -1
 trash_side = -1
 for i=0,5 do
     inv_name = component.transposer.getInventoryName(i)
-    for _, iname = ipairs(inv_names) do
+    for _, iname in ipairs(inv_names) do
         if inv_name == iname then
             inv_side = i
+            break
         end
     end
 
-    for _, iname = ipairs(trash_names) do
+    for _, iname in ipairs(trash_names) do
         if inv_name == iname then
             trash_side = i
+            break
         end
     end
 end
@@ -30,14 +32,14 @@ end
 -- testing
  
  
---scanner_side = 3
-if scanner_side < 0 then
-  print("Could not find storage scanner")
+--inv_side = 3
+if inv_side < 0 then
+  print("Could not find inventory")
   os.exit()
 end
  
-inv_size = component.transposer.getInventorySize(scanner_side)
-print("Using storage scanner in side " .. tostring(scanner_side) .. " with size " .. tostring(inv_size))
+inv_size = component.transposer.getInventorySize(inv_side)
+print("Using inventory in side " .. tostring(inv_side) .. " with size " .. tostring(inv_size))
 if trash_side >= 0 then
   print("Using trash on side " .. tostring(trash_side))
 else
@@ -46,7 +48,7 @@ end
  
 item_aggregates = {}
 for i=1,inv_size do
-  stack = component.transposer.getStackInSlot(scanner_side, i)
+  stack = component.transposer.getStackInSlot(inv_side, i)
   if stack ~= nil then
     item_count = item_aggregates[stack.name] or 0
     item_count = item_count + stack.size
