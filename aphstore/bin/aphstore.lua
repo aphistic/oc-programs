75fb52b588f47a3bb87e2947dedae00ef75f2c62
Erik Davidson <erik@erikd.org>
2018-12-16 21:58:19 -0600

    Update aphstore.lua

--- aphstore/bin/aphstore.lua
+++ aphstore/bin/aphstore.lua
@@ -5,10 +5,10 @@ local sides = require("sides")
 inv_names = {
     "storagedrawers:controller",
 }
+trash_max = 10000
 trash_names = {
     "extrautils2:trashchest",
 }
-
 
 inv_side = -1
 trash_side = -1
@@ -29,10 +29,6 @@ for i=0,5 do
     end
 end
 
--- testing
- 
- 
---inv_side = 3
 if inv_side < 0 then
   print("Could not find inventory")
   os.exit()
@@ -41,22 +37,21 @@ end
 inv_size = component.transposer.getInventorySize(inv_side)
 print("Using inventory in side " .. tostring(inv_side) .. " with size " .. tostring(inv_size))
 if trash_side >= 0 then
-  print("Using trash on side " .. tostring(trash_side))
+    print("Using trash on side " .. tostring(trash_side))
 else
-  print("Couldn't find trash to use")
+    print("Couldn't find trash to use")
 end
  
 item_aggregates = {}
 for i=1,inv_size do
-  stack = component.transposer.getStackInSlot(inv_side, i)
-  if stack ~= nil then
-    item_count = item_aggregates[stack.name] or 0
-    item_count = item_count + stack.size
-    item_aggregates[stack.name] = item_count
-    print(stack.name .. " - " .. tostring(item_count))
-  end
+    stack = component.transposer.getStackInSlot(inv_side, i)
+    if stack ~= nil then
+        item_aggregates[stack.name] = (item_aggregates[stack.name] or 0) + stack.size
+    end
 end
  
 for k, v in pairs(item_aggregates) do
-  print(k .. " - " .. tostring(v))
+    if v > trash_max then
+        print(k .. " - " .. tostring(v))
+    end
 end
